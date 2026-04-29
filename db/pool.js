fecda79bf5747fb0071cd00f9bad769907ab2304
John Walter Munene Njeru <midwaymaster10@gmail.com>
2026-04-29 15:34:01 +0300

    Switch to SSL connection for hosting!

--- db/pool.js
+++ db/pool.js
@@ -4,9 +4,8 @@ require('dotenv').config({ path: path.join(__dirname, '../.env')});
 const { Pool } = require('pg');
 
 module.exports = new Pool({ 
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_NAME,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_PORT,
+    connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? { rejectUnauthorized: false }
+    : false
 });
