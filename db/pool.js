4d710166e47cb80523ea7231365d99ddd875d65c
John Walter Munene Njeru <104344424+john-walter-munene@users.noreply.github.com>
2026-04-29 16:00:33 +0300

    Refactor database pool configuration

--- db/pool.js
+++ db/pool.js
@@ -1,11 +1,10 @@
-const path = require('node:path');
-require('dotenv').config({ path: path.join(__dirname, '../.env')});
-
 const { Pool } = require('pg');
 
-module.exports = new Pool({ 
+const isProduction = process.env.NODE_ENV === 'production';
+
+module.exports = new Pool({
     connectionString: process.env.DATABASE_URL,
-  ssl: isProduction
-    ? { rejectUnauthorized: false }
-    : false
+    ssl: isProduction
+        ? { rejectUnauthorized: false }
+        : false
 });
